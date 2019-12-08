e31f6018f4ee9f37f39fa0400f8d94d08417c126
Jeremy Yap <dcsyjlj@nus.edu.sg>
2019-12-08 13:35:00 +0800

    Tweak day 4 solution

--- 2019/4.cr
+++ 2019/4.cr
@@ -1,6 +1,6 @@
 def passwords(first, last)
   (first..last).count do |x|
-    digits = x.to_s.split("").map(&.to_i)
+    digits = x.to_s.chars.map(&.to_i)
     is_ascending = digits.each_cons(2).all? { |cons| cons[0] <= cons[1] }
     digits.unshift(0).push(0)
     has_pair = digits.each_cons(4).any? { |cons| cons[0] != cons[1] && cons[1] == cons[2] && cons[2] != cons[3] }
