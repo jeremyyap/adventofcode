4e3fabfaac50b4b71f10f0e896d2500b8a058993
Jeremy Yap <jeremy.yapjl@gmail.com>
2020-12-06 13:33:08 +0800

    Merge part solutions for day 1 and day 2

---
+++ 2020/1.cr
@@ -0,0 +1,40 @@
+class Program
+  @inputs: Array(Int32)
+
+  def initialize
+    @inputs = File.read("1.txt").split.map(&.to_i)
+  end
+
+  def part_1
+    set = Set(Int32).new
+    @inputs.each do |x|
+      y = 2020 - x
+      if set.includes?(y)
+        return y * x
+      end
+
+      set.add(x)
+    end
+  end
+
+  def part_2
+    set = Set(Int32).new
+    @inputs.each_with_index do |x, i|
+      @inputs.each_with_index do |y, j|
+        if (j > i)
+          z = 2020 - x - y
+          return x * y * z if set.includes?(z)
+        end
+      end
+
+      set.add(x)
+    end
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
---
+++ 2020/2.cr
@@ -0,0 +1,37 @@
+inputs = File.read("2.txt").split("\n")[0..-2]
+
+class Program
+  @inputs: Array(String)
+
+  def initialize
+    @inputs = File.read("2.txt").split("\n")[0..-2]
+  end
+
+  def part_1
+    @inputs.count do |line|
+      range, letter, password = line.split(' ')
+      min, max = range.split('-').map(&.to_i)
+      char = letter[0]
+
+      occurrences = password.chars.count(char)
+      min <= occurrences && occurrences <= max
+    end
+  end
+
+  def part_2
+    @inputs.count do |line|
+      range, letter, password = line.split(' ')
+      left, right = range.split('-').map(&.to_i)
+      char = letter[0]
+
+      (password[left - 1] == char) ^ (password[right - 1] == char)
+    end
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
