2afa69910bac3af22974372320a756769fe95e31
Jeremy Yap <jeremy.yapjl@gmail.com>
2020-12-10 14:21:51 +0800

    Add solution for day 10

---
+++ 2020/10.cr
@@ -0,0 +1,32 @@
+class Program
+  @inputs: Array(Int32)
+
+  def initialize
+    @inputs = File.read("10.txt")[0..-2].split.map(&.to_i)
+  end
+
+  def part_1
+    adapters = @inputs.sort
+    differences = adapters.each_cons(2).to_a.map { |(x, y)| y - x }
+    (differences.count(1) + 1) * (differences.count(3) + 1)
+  end
+
+  def part_2
+    adapters = @inputs.sort
+    counts = adapters.map { |x| 0_i64 }
+    counts[counts.size - 1] = 1
+    (adapters.size - 2).downto(0) do |i|
+      counts[i] = [i+1, i+2, i+3]
+        .select { |j| j < adapters.size && adapters[j] - adapters[i] <= 3 }
+        .sum { |j| counts[j] }
+    end
+    counts[0] + counts[1] + counts[2]
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
