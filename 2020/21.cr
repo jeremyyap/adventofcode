d292226c73b0b51c63c5e33005c909a88c1dd448
Jeremy Yap <jeremy.yapjl@gmail.com>
2020-12-23 12:57:10 +0800

    Add solutions for day 21 and 22

---
+++ 2020/21.cr
@@ -0,0 +1,59 @@
+class Program
+  @input : Array(String)
+
+  def initialize
+    @input = File.read("21.txt")[0..-2].split("\n")
+  end
+
+  def execute
+    ingredient_food_map = Hash(String, Set(Int32)).new { Set(Int32).new }
+    allergen_food_map = Hash(String, Set(Int32)).new { Set(Int32).new }
+
+    # Food contains allergen but does not contains ingredient => ingredient does not contain allergen
+    @input.each_with_index do |food, food_id|
+      ingredients_list, allergens_list = food[0..-2].split(" (contains ")
+      ingredients = ingredients_list.split(' ')
+      allergens = allergens_list.split(", ")
+
+      ingredients.each do |ingredient|
+        ingredient_food_map[ingredient] = ingredient_food_map[ingredient].add(food_id)
+      end
+
+      allergens.each do |allergen|
+        allergen_food_map[allergen] = allergen_food_map[allergen].add(food_id)
+      end
+    end
+
+    # Part 1
+    safe_ingredients_count = ingredient_food_map.keys.reject do |ingredient|
+      allergen_food_map.any? do |_, food_ids|
+        food_ids.all? { |food_id| ingredient_food_map[ingredient].includes?(food_id) }
+      end
+    end.sum { |ingredient| ingredient_food_map[ingredient].size }
+    puts safe_ingredients_count
+
+    allergic_ingredients = ingredient_food_map.keys.select do |ingredient|
+      allergen_food_map.any? do |_, food_ids|
+        food_ids.all? { |food_id| ingredient_food_map[ingredient].includes?(food_id) }
+      end
+    end
+
+    # Part 2
+    sorted_allergens = allergen_food_map.keys.sort
+    allergic_ingredients.permutations.each do |permutation|
+      correct = true
+      permutation.each_with_index do |ingredient, idx|
+        allergen = sorted_allergens[idx]
+        food_ids = allergen_food_map[allergen]
+        correct = false unless food_ids.all? { |food_id| ingredient_food_map[ingredient].includes?(food_id) }
+      end
+
+      if correct
+        puts permutation.join(',')
+        break
+      end
+    end
+  end
+end
+
+Program.new.execute
---
+++ 2020/22.cr
@@ -0,0 +1,66 @@
+class Program
+  @input : Array(String)
+
+  def initialize
+    @input = File.read("22.txt")[0..-2].split("\n\n")
+  end
+
+  def score(d1, d2)
+    deck = d1.empty? ? d2 : d1
+    score = deck.reverse.each_with_index.sum { |card, idx| card * (idx + 1) }
+  end
+
+  def part_1
+    d1 = @input[0].split("\n")[1..-1].map(&.to_i)
+    d2 = @input[1].split("\n")[1..-1].map(&.to_i)
+
+    while !d1.empty? && !d2.empty?
+      c1 = d1.shift
+      c2 = d2.shift
+
+      if c1 > c2
+        d1 += [c1, c2]
+      else
+        d2 += [c2, c1]
+      end
+    end
+
+    score(d1, d2)
+  end
+
+  def play(d1, d2) : Bool # P1 wins
+    states = Set(Tuple(Array(Int32), Array(Int32))).new
+    while !d1.empty? && !d2.empty?
+      return true if states.includes?({ d1, d2 })
+      states.add({ d1.clone, d2.clone })
+      c1 = d1.shift
+      c2 = d2.shift
+
+      p1_win = c1 > c2
+      if d1.size >= c1 && d2.size >= c2
+        p1_win = play(d1.clone[0...c1], d2.clone[0...c2])
+      end
+
+      if p1_win
+        d1.push(c1, c2)
+      else
+        d2.push(c2, c1)
+      end
+    end
+    d1.empty? ? false : true
+  end
+
+  def part_2
+    p1 = @input[0].split("\n")[1..-1].map(&.to_i)
+    p2 = @input[1].split("\n")[1..-1].map(&.to_i)
+    play(p1, p2)
+    score(p1, p2)
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
