32bb4e6ccd542463a5716ac8c124c2cc52b6facb
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-12-02 13:17:07 +0800

    Add solution for day 2

---
+++ 2021/2.cr
@@ -0,0 +1,49 @@
+class Program
+  @inputs: Array(Tuple(String, Int32))
+
+  def initialize
+    @inputs = File.read("2.txt")[0..-2].split("\n").map do |line|
+      command, x = line.split(" ")
+      { command, x.to_i }
+    end
+  end
+
+  def part_1
+    horizontal = 0
+    depth = 0
+    @inputs.each do |command, x|
+      if command == "forward"
+        horizontal += x
+      elsif command == "down"
+        depth += x
+      elsif command == "up"
+        depth -= x
+      end
+    end
+    horizontal * depth
+  end
+
+  def part_2
+    horizontal = 0
+    depth = 0
+    aim = 0
+    @inputs.each do |command, x|
+      if command == "forward"
+        horizontal += x
+        depth += aim * x
+      elsif command == "down"
+        aim += x
+      elsif command == "up"
+        aim -= x
+      end
+    end
+    horizontal * depth
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
