b3e5013e0e4dc7bbede3ca33aaf14ad4cae7ee71
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-12-05 14:29:53 +0800

    Add solution for day 5

---
+++ 2021/5.cr
@@ -0,0 +1,51 @@
+class Point
+  getter x : Int32
+  getter y : Int32
+  def initialize(x : Int32, y : Int32)
+    @x = x
+    @y = y
+  end
+end
+
+alias Segment = Tuple(Point, Point)
+
+class Program
+  @inputs: Array(Segment)
+  @map: Hash(Tuple(Int32, Int32), Int32)
+
+  def initialize
+    @inputs = File.read("5.txt")[0..-2].split("\n").map do |line|
+      p1, p2 = line.split(" -> ")
+      p1x, p1y = p1.split(",").map(&.to_i)
+      p2x, p2y = p2.split(",").map(&.to_i)
+      { Point.new(p1x, p1y), Point.new(p2x, p2y) }
+    end
+    @map = Hash(Tuple(Int32, Int32), Int32).new(0)
+  end
+
+  def iterator(first : Int32, last : Int32)
+    return Iterator.of(first) if first == last
+    first < last ? (first..last).each : first.downto(last)
+  end
+
+  def plot_points(s : Segment)
+    x_range = iterator(s[0].x, s[1].x)
+    y_range = iterator(s[0].y, s[1].y)
+    x_range.zip(y_range).each { |x, y| @map[{x,y}] += 1 }
+  end
+
+  def execute
+    perpendiculars, diagonals =
+      @inputs.partition { |s| s[0].x == s[1].x || s[0].y == s[1].y }
+
+    # part 1
+    perpendiculars.each { |s| plot_points(s) }
+    puts @map.count { |k, v| v > 1 }
+
+    # part 2
+    diagonals.each { |s| plot_points(s) }
+    puts @map.count { |k, v| v > 1 }
+  end
+end
+
+Program.new.execute
