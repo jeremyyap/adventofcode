e8f0bb50066b94547b06ec0aa185e7ec8f0c3798
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-12-11 13:32:29 +0800

    Add solutions for day 9 and 10

---
+++ 2021/10.cr
@@ -0,0 +1,53 @@
+class Program
+  @inputs: Array(Array(Char))
+
+  def initialize
+    @inputs = File.read("10.txt").strip.split("\n").map { |line| line.chars }
+  end
+
+  @@pairs_map = { '(' => ')', '[' => ']', '{' => '}', '<' => '>' }
+
+  def part_1
+    points_map = { ')' => 3, ']' => 57, '}' => 1197, '>' => 25137 }
+
+    @inputs.sum do |chars|
+      stack = [] of Char
+      score = 0
+      chars.each do |c|
+        if @@pairs_map.keys.includes?(c)
+          stack.push(@@pairs_map[c])
+        else
+          break score = points_map[c] if stack.pop() != c
+        end
+      end
+      score
+    end
+  end
+
+  def part_2
+    points_map = { ')' => 1, ']' => 2, '}' => 3, '>' => 4 }
+
+    scores = @inputs.map do |chars|
+      stack = [] of Char
+      corrupted = false
+      chars.each do |c|
+        if @@pairs_map.keys.includes?(c)
+          stack.push(@@pairs_map[c])
+        else
+          break corrupted = true if stack.pop() != c
+        end
+      end
+
+      stack.reverse.reduce(0_i64) { |score, c| score * 5 + points_map[c] } unless corrupted
+    end.compact.sort
+
+    scores[scores.size // 2]
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
---
+++ 2021/9.cr
@@ -0,0 +1,44 @@
+class Program
+  @inputs: Array(Array(Int32))
+
+  def initialize
+    @inputs = File.read("9.txt").strip.split("\n").map { |line| line.chars.map { |c| c.to_i } }
+  end
+
+  def part_1
+    @inputs.each_with_index.sum do |row, y|
+      row.each_with_index.sum do |height, x|
+        (
+          (x == 0 || @inputs[y][x-1] > height) &&
+          (y == 0 || @inputs[y-1][x] > height) &&
+          (y == @inputs.size - 1 || @inputs[y+1][x] > height) &&
+          (x == row.size - 1 || @inputs[y][x+1] > height)
+        ) ? 1 + height : 0
+      end
+    end
+  end
+
+  def flood_fill(x : Int32, y : Int32)
+    return 0 if x < 0 || y < 0 || x >= @inputs[0].size || y >= @inputs.size || @inputs[y][x] == 9
+
+    @inputs[y][x] = 9
+    return 1 + flood_fill(x-1, y) + flood_fill(x, y-1) + flood_fill(x+1, y) + flood_fill(x, y+1)
+  end
+
+  def part_2
+    basins = [] of Int32
+    @inputs.each_with_index do |row, y|
+      row.each_with_index do |height, x|
+        basins << flood_fill(x, y) if height != 9
+      end
+    end
+    basins.sort.last(3).product
+  end
+
+  def execute
+    puts part_1
+    puts part_2
+  end
+end
+
+Program.new.execute
