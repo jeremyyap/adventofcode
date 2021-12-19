7f70bdea284bb60c508b89ff4aeae4678e511794
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-12-20 00:45:40 +0800

    Refactor 2019 day 7 to use shared Intcode program

--- 2019/7.cr
+++ 2019/7.cr
@@ -1,78 +1,31 @@
+require "./intcode"
+
 class Program
-  @instructions: Array(Int32)
-  @parameter_modes: Array(Int8)
-  @input: Channel(Int32)
-  @output: Channel(Int32)
+  @instructions: Array(Int64)
 
-  def initialize(input : Channel(Int32), output : Channel(Int32))
-    @instructions = File.read("7.txt").split(',').map(&.to_i)
-    @parameter_modes = [] of Int8
-    @pos = 0
-    @input = input
-    @output = output
+  def initialize
+    @instructions = File.read("7.txt").chomp.split(',').map(&.to_i64)
   end
 
-  def get_param
-    parameter_mode = @parameter_modes.pop? || 0
-    val = parameter_mode == 1 ? @instructions[@pos] : @instructions[@instructions[@pos]]
-    @pos += 1
-    val
-  end
-
-  def set_param(value)
-    @parameter_modes.pop?
-    @instructions[@instructions[@pos]] = value
-    @pos += 1
+  def amplify(phase_settings : Array(Int32), channels : Array(Channel(Int64)))
+    phase_settings.each_with_index do |phase, i|
+      spawn { Intcode.new(@instructions, channels[i], channels[i+1]).execute }
+      channels[i].send(phase.to_i64)
+    end
+    channels.first.send(0_i64)
+    Fiber.yield
+    channels.last.receive
   end
 
   def execute
-    while true
-      instruction = @instructions[@pos]
-      @parameter_modes = (instruction // 100).to_s.split("").map(&.to_i8)
-      @pos += 1
-      opcode = instruction % 100
+    # part 1
+    channels = Array.new(6) { Channel(Int64).new }
+    puts [0,1,2,3,4].permutations.max_of { |permutation| amplify(permutation, channels) }
 
-      case opcode
-        when 1
-          set_param(get_param + get_param)
-        when 2
-          set_param(get_param * get_param)
-        when 3
-          set_param(@input.receive)
-        when 4
-          @output.send(get_param)
-        when 5
-          @pos = get_param == 0 ? @pos + 1 : get_param
-        when 6
-          @pos = get_param == 0 ? get_param : @pos + 1
-        when 7
-          set_param(get_param < get_param ? 1 : 0)
-        when 8
-          set_param(get_param == get_param ? 1 : 0)
-        when 99
-          break
-        else
-          raise "Invalid opcode"
-      end
-    end
+    # part 2
+    channels[5] = channels.first
+    puts [5,6,7,8,9].permutations.max_of { |permutation| amplify(permutation, channels) }
   end
 end
 
-def amplify(phase_settings : Array(Int32))
-  channels = phase_settings.map { Channel(Int32).new }
-
-  phase_settings.each_with_index do |phase, index|
-    spawn do
-      Program.new(channels[index], channels[(index + 1) % phase_settings.size]).execute
-    end
-    channels[index].send(phase)
-  end
-
-  channels.first.send(0)
-  Fiber.yield
-  channels.first.receive
-end
-
-inputs = [5, 6, 7, 8, 9]
-
-puts inputs.each_permutation.max_of { |permutation| amplify(permutation) }
+Program.new.execute
