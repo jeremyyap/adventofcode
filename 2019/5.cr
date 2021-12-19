fdeefc785ee05f215758013d3324bae0007dd902
Jeremy Yap <jeremy.yapjl@gmail.com>
2021-12-20 00:45:16 +0800

    Refactor 2019 day 5 to use shared Intcode program

--- 2019/5.cr
+++ 2019/5.cr
@@ -1,57 +1,35 @@
+require "./intcode"
+
 class Program
-  @instructions: Array(Int32)
-  @parameter_modes: Array(Int8)
+  @instructions: Array(Int64)
+  @input: Channel(Int64)
+  @output: Channel(Int64)
 
   def initialize
-    @instructions = File.read("5.txt").split(',').map(&.to_i)
-    @parameter_modes = [] of Int8
-    @pos = 0
+    @instructions = File.read("5.txt").chomp.split(',').map(&.to_i64)
+    @input = Channel(Int64).new
+    @output = Channel(Int64).new
   end
 
-  def get_param
-    parameter_mode = @parameter_modes.pop? || 0
-    val = parameter_mode == 1 ? @instructions[@pos] : @instructions[@instructions[@pos]]
-    @pos += 1
-    val
+  def part_1
+    instructions = @instructions.clone
+    spawn { @input.send(1) }
+    last = 0
+    spawn { while last == 0 { last = @output.receive } end }
+    Intcode.new(instructions, @input, @output).execute
+    last
   end
 
-  def set_param(value)
-    @parameter_modes.pop?
-    @instructions[@instructions[@pos]] = value
-    @pos += 1
+  def part_2
+    instructions = @instructions.clone
+    spawn { @input.send(5) }
+    spawn { Intcode.new(instructions, @input, @output).execute }
+    @output.receive
   end
 
   def execute
-    while true
-      instruction = @instructions[@pos]
-      @parameter_modes = (instruction // 100).to_s.split("").map(&.to_i8)
-      @pos += 1
-      opcode = instruction % 100
-
-      case opcode
-        when 1
-          set_param(get_param + get_param)
-        when 2
-          set_param(get_param * get_param)
-        when 3
-          puts "Enter input:"
-          set_param(gets.not_nil!.to_i)
-        when 4
-          puts get_param
-        when 5
-          @pos = get_param == 0 ? @pos + 1 : get_param
-        when 6
-          @pos = get_param == 0 ? get_param : @pos + 1
-        when 7
-          set_param(get_param < get_param ? 1 : 0)
-        when 8
-          set_param(get_param == get_param ? 1 : 0)
-        when 99
-          break
-        else
-          raise "Invalid opcode"
-      end
-    end
+    puts part_1
+    puts part_2
   end
 end
 
